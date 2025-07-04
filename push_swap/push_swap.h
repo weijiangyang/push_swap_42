ac8a7e228ee4880665141360d80dd29199a32b35
Weijiang Yang <weiyang@f4r7s1.clusters.42paris.fr>
2025-07-04 17:40:25 +0200

    commit

--- push_swap/push_swap.h
+++ push_swap/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: weiyang <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/03 12:09:34 by weiyang           #+#    #+#             */
-/*   Updated: 2025/07/04 15:20:53 by weiyang          ###   ########.fr       */
+/*   Updated: 2025/07/04 16:45:00 by weiyang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,18 +39,19 @@ void    rrotate(t_list **list);
 void    rra(t_list **list);
 void    rrb(t_list **list);
 void    rrr(t_list **list_a, t_list **list_b);
-int     ft_list_size(t_list *list);
 int     is_sorted_asc(t_list *list);
 int     is_sorted_desc(t_list *list);
-int     ft_strlen(char *str);
 int     count_max_bits(t_list *list);
 int    sort(t_list **list_a, t_list **list_b);
 t_list *new_node(int value);
 long    ft_atoi(char *s);
 int     is_valid_int(char *str);
 int     has_duplicate(t_list *list);
+int	ft_list_size(t_list *list);
 void assign_indexes(t_list *lst);
 t_list  *parse_list(int argc, char **argv);
+int     get_max_index(t_list *list);
+int     bring_max_to_top(t_list **list, int max_index);
 
 
 
--- push_swap/sort.c
+++ push_swap/sort.c
@@ -6,109 +6,192 @@
 /*   By: weiyang <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/04 11:23:27 by weiyang           #+#    #+#             */
-/*   Updated: 2025/07/04 15:30:47 by weiyang          ###   ########.fr       */
+/*   Updated: 2025/07/04 17:19:47 by weiyang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	is_sorted_asc(t_list *list)
+int is_sorted_asc(t_list *list)
 {
-	while (list && list->next)
-	{
-		if (list->index > list->next->index)
-			return (0);
-		list = list->next;
-	}
-	return (1);
+        while (list && list->next)
+        {
+                if (list->index > list->next->index)
+                        return (0);
+                list = list->next;
+        }
+        return (1);
 }
 
-int	is_sorted_desc(t_list *list)
+int is_sorted_desc(t_list *list)
 {
-	while (list && list->next)
-	{
-		if (list->index < list->next->index)
-			return (0);
-		list = list->next;
-	}
-	return (1);
+        while (list && list->next)
+        {
+                if (list->index < list->next->index)
+                        return (0);
+                list = list->next;
+        }
+        return (1);
 }
 
-int	ft_strlen(char *str)
+int get_max_index(t_list *list)
 {
-	int	len;
+        int max_index;
+        t_list *first;
 
-	len = 0;
-	while (*str)
-	{
-		len++;
-		str++;
-	}
-	return (len);
+        first = list;
+        if (!first)
+                return (0);
+        max_index = first->index;
+        while (first)
+        {
+                if (max_index < first->index)
+                        max_index = first->index;
+                first = first->next;
+        }
+        return (max_index);
 }
 
-int	count_max_bits(t_list *list)
+int count_max_bits(t_list *list)
 {
-	int	max_bits;
-	int	max_index;
-	t_list	*first;
+        int max_bits;
+        int max_index;
+        t_list *first;
 
-	max_bits = 0;
-	first = list;
-	if(!first)
-		return (0);
-	max_index = first->index;
-	while (first)
-	{
-		if (max_index < first->index)
-			max_index = first->index;
-		first = first -> next;
-	}
-	while ((max_index >> max_bits)!= 0)
-		max_bits++;
-	return (max_bits);
+        max_bits = 0;
+        first = list;
+        if (!first)
+                return (0);
+        max_index = get_max_index(list);
+        while ((max_index >> max_bits) != 0)
+                max_bits++;
+        return (max_bits);
 }
- 		
-int	sort(t_list **list_a, t_list **list_b)
+
+int     bring_max_to_top(t_list **list, int max_index)
 {
-	int	size;
-	int	i;
-	int	max_bits;
-	int	j;
-	int	count;
+        int     size;
+        int     pos;
+        t_list  *current;
+        int     i;
+        int     count;
 
-	count = 0;
-	max_bits = count_max_bits(*list_a);
-	j = 0;
+        count = 0;
+        i = 0;
+        pos = 0;
+        size = ft_list_size(*list);
+        current = *list;
+        while (current && current->index != max_index)
+        {
+                current = current->next;
+                pos++;
+        }
+        if (pos < size / 2)
+        {
+                while (i < pos)
+                {
+                        rb(list);
+                        count++;
+                        i++;
+                }
+        }
+        else
+        {
+                while (i < size - pos)
+                {
+                        rrb(list);
+                        count++;
+                        i++;
+                }
+        }
+        return (count);
 
-	while (j < max_bits)
-	{
-		i = 0;
-		size = ft_list_size(*list_a);
-		while(i < size)
-		{
-			if (is_sorted_asc(*list_a))
-				break;
-			if (((*list_a)->index >> j)& 1) 
-			{
-				ra(list_a);
-				count++;
-			}
-			else
-			{
-				if (i == size -1 )
-					break;
-				pb(list_b, list_a);
-				count++;
-			}
-			i++;
-		}			
-		while (*list_b)
-		{
-			pa(list_a, list_b);
-			count++;
-		}
-		j++;
-	}
-	return (count);
+}
+
+/*int	sort(t_list **list_a, t_list **list_b)
+{
+        int	size;
+        int	i;
+        int	max_bits;
+        int	j;
+        int	count;
+
+        count = 0;
+        max_bits = count_max_bits(*list_a);
+        j = 0;
+
+        while (j < max_bits)
+        {
+                i = 0;
+                size = ft_list_size(*list_a);
+                while(i < size)
+                {
+                        if (is_sorted_asc(*list_a))
+                                break;
+                        if (((*list_a)->index >> j)& 1)
+                        {
+                                ra(list_a);
+                                count++;
+                        }
+                        else
+                        {
+                                if (i == size -1 )
+                                        break;
+                                pb(list_b, list_a);
+                                count++;
+                        }
+                        i++;
+                }
+                while (*list_b)
+                {
+                        pa(list_a, list_b);
+                        count++;
+                }
+                j++;
+        }
+        return (count);
+}*/
+
+int sort(t_list **list_a, t_list **list_b)
+{
+        int     list_size = ft_list_size(*list_a);
+        int chunk_size = list_size / 5;
+        int current_chunk = 1;
+        int limit = chunk_size * current_chunk;
+        int    count_in_b = 0;
+        int     count_actions;
+
+        count_actions = 0;
+        while (*list_a)
+        {
+                if ((*list_a)->index <= limit)
+                {
+                        pb(list_b, list_a);
+                        count_in_b++;
+                        count_actions++;
+                        if ((*list_b)->index < limit - (chunk_size / 2))
+                        {
+                                count_actions++;
+                                ra(list_b); // pour mieux répartir
+                        }   
+                }
+                else
+                {
+                        count_actions++;
+                        ra(list_a);
+                }
+                if (count_in_b == limit) // passer au prochain chunk
+                        current_chunk++;
+                limit = chunk_size * current_chunk;
+        }
+
+        // ensuite : vider B en mettant le max au top
+        while (*list_b)
+        {
+                int max = get_max_index(*list_b);
+                bring_max_to_top(list_b, max);
+                pa(list_a, list_b);
+                count_actions++;
+        }
+        return (count_actions);
 }
